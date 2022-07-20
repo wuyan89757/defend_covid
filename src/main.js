ed3f272e54ef65d81a3c89a40f73f5000fcfb6ec
wuyan89757 <108069440+wuyan89757@users.noreply.github.com>
2022-07-20 15:30:54 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -22,6 +22,29 @@ import './styles/index.scss'
 import fmtDateTime from './filter/time' // 导入过滤器
 import borthdate from './filter/borth'
 import VueSocketIo from 'vue-socket.io'
+/*
+导入疫情数据
+overall：病毒研究情况以及全国疫情概览
+provinceName：有数据条目的国家、省份、地区、直辖市列表
+area：中国所有省份、地区或直辖市及世界其他国家的所有疫情信息变化的时间序列数据（精确到市）
+news：所有与疫情有关的新闻信息，包含数据来源以及数据来源链接
+rumors：与疫情有关的谣言以及丁香园的辟谣
+*/
+import overall from './api/overall'
+Vue.prototype.$overall = overall
+
+import provinceName from './api/provinceName'
+Vue.prototype.$provinceName = provinceName
+
+import area from './api/area'
+Vue.prototype.$area = area
+
+import news from './api/news'
+Vue.prototype.$news = news
+
+import rumors from './api/rumors'
+Vue.prototype.$rumors = rumors
+
 
 import './styles/face.css'
 import './styles/face.js'
