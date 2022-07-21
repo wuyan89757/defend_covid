24f34d1c6d9a4b35e9e54a911a32eb856a844e61
TOLIE-OFFICIAL <2377397583@qq.com>
2022-07-21 11:52:14 +0800

    newBranch:push test

--- src/api/covid/overall.js
+++
@@ -1,9 +0,0 @@
-import { get } from '@/utils/request'
-
-const overall = {
-    getDataInfo(params) {
-       return get('https://lab.isaaclin.cn/nCoV/api/overall',params)
-    }
-}
-
-export default overall;
